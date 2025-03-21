5ec3883079039acde26384cf968dcd93709b21da
danaguil <danaguilarfabian@gmail.com>
2025-03-21 10:31:51 -0700

    Completed the simple process program

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
+/*
+    Program will create a simple process with a parent with a child
+ */
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+    /*
+        Data type that will hold id(int) for process
+        - can view ID with getpid()
+    */
+    printf("Before fork\n");
+
+    pid_t proc1 = fork(); // forking proc, will include the unique iD
+
+    // if the process is a child
+    if (proc1 == 0) {
+        printf("I am a child with ID %d\n", getpid());
+        printf("My parent's ID is %d\n", getppid());
+    } else { // if the proc is a parent
+        printf("My child's ID is %d\n", proc1);
+        printf("I am parent having an ID %d\n", getpid());
+    }
+
 }
